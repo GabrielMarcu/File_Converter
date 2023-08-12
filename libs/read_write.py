f18ad2d1c2c411d46b5ed0ea0844742233ec9b40
GabrielMarcu <gabriel.marcu93@gmail.com>
2023-08-12 18:44:02 +0300

    read csv and text files implemented

--- libs/read_write.py
+++ libs/read_write.py
@@ -1,12 +1,19 @@
 """Module used for reading and writing files"""
+import csv
 
 
-def read_csv(path: str) ->list[list]:
-    pass
+def read_csv(path: str) -> list[list]:
+    with open(path, 'r') as fr:
+        reader = csv.reader(fr)
+        rows = [row for row in reader]
+    return rows
 
 
-def read_txt(path: str) -> list:
-    pass
+def read_txt(path: str) -> list[list]:
+    with open(path, 'r') as fr:
+        stripped = (line.strip() for line in fr)
+        lines = [line.split(",") for line in stripped if line]
+    return lines
 
 
 def write_csv(path: str) -> None:
@@ -16,3 +23,7 @@ def write_csv(path: str) -> None:
 def write_txt(path: str) -> None:
     pass
 
+
+if __name__ == '__main__':
+    print(read_txt(r"D:\Gabriel\Curs_Python\Team_Project\Read_File\test.txt"))
+    print(read_csv(r"D:\Gabriel\Curs_Python\Password Manager\userdata.csv"))
