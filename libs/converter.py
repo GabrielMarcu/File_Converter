82795e1ce642a4153c834d5099870e3e4c0b36ee
unknown <grigorec749@gmail.com>
2023-09-03 20:03:30 -0700

    convertor implemented'

--- libs/converter.py
+++ libs/converter.py
@@ -1,13 +1,56 @@
 """Docstring"""
 
+import csv
+import os
 
-def convert_to_txt():
-    pass
+
+def text_to_csv(input_path, output_path) -> None:
+    """
+    This function converts the text file into a csv file
+    """
+    if os.path.splitext(input_path)[1] != '.txt':
+        raise ValueError('Wrong input file. It must be a text file!')
+    if os.path.splitext(output_path)[1] != '.csv':
+        raise ValueError('Wrong output file. It must be a csv file!')
+
+    with open(input_path, 'r') as text_file:
+        lines = text_file.readlines()
+
+    with open(output_path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        for line in lines:
+            csv_writer.writerow([line.strip()])
 
 
-def convert_to_csv():
-    pass
+def csv_to_text(input_path, output_path) -> None:
+    """
+    This function converts a csv file into a text file
+    """
+    if os.path.splitext(input_path)[1] != '.csv':
+        raise ValueError('Wrong input file. It must be a csv file!')
+    if os.path.splitext(output_path)[1] != '.txt':
+        raise ValueError('Wrong output file. It must be a txt file!')
+
+    with open(input_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        lines = [",".join(row) for row in csv_reader]
+
+    with open(output_path, 'w') as text_file:
+        text_file.write("\n".join(lines))
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        input_file = input('input file: ')
+        output_file = input('output file: ')
+        text_to_csv(input_file, output_file)
+    except ValueError as e:
+        print(f'Error -> {e}')
+
+    try:
+        input_csv_file = input('csv file: ')
+        output_csv_file = input('text file: ')
+        csv_to_text(input_csv_file, output_csv_file)
+    except ValueError as e:
+        print(f'Error -> {e}')
+
